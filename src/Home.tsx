60216e26c18d44f38d5ae5fa9bb1886b62b9bf00
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-09-02 04:05:42 +0530

    demo

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,13 +9,13 @@ import TestimonialsSection from './components/TestimonialsSection';
 import RiskFreeSection from './components/RiskFreeSection';
 import FAQSection from './components/FAQSection';
 import FooterSection from './components/FooterSection';
-// import DemoVideo from './components/DemoVIdeo';
+import DemoVideo from './components/DemoVIdeo';
 function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-black via-[#001a33] to-[#003366] overflow-visible">
       <Header />
       <Hero />
-      {/* <DemoVideo/> */}
+      <DemoVideo/>
       <Features />
       <HowItWorks />
       <Pricing />
