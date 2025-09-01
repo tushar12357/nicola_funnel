7b78798c314fd3ee12596d95647a58eff52df0ad
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-09-02 03:48:49 +0530

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import Header from './components/Header';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Pricing from './components/Pricing';
-import Hero from './components/Hero';
-import SolutionsSection from './components/SolutionsSections';
-import CaseStudySection from './components/CaseStudySection';
-import TestimonialsSection from './components/TestimonialsSection';
-import RiskFreeSection from './components/RiskFreeSection';
-import FAQSection from './components/FAQSection';
-import FooterSection from './components/FooterSection';
+
+import Home from './Home';
 // import DemoVideo from './components/DemoVIdeo';
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-black via-[#001a33] to-[#003366] overflow-visible">
-      <Header />
-      <Hero />
-      {/* <DemoVideo/> */}
-      <Features />
-      <HowItWorks />
-      <Pricing />
-      <SolutionsSection />
-      <CaseStudySection />
-      <TestimonialsSection />
-      <RiskFreeSection/>
-      <FAQSection/>
-      <FooterSection/>
-    </div>
+   <Home/>
   );
 }
 
